a824011790f948a3fcce3a4cb0039ec02357117e
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-08-08 11:12:48 -0300

    define return type of DashboardController method invoke

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    public function __invoke()
+    public function __invoke(): View
     {
         $questions = Question::all();
         return view('dashboard', [
