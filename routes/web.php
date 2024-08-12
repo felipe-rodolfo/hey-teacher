93214ef76985d1f58aeb99e38ae048c40e481455
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-08-12 20:55:46 -0300

    be able to like a question

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\{DashboardController, ProfileController, QuestionController};
+use App\Http\Controllers\{DashboardController, ProfileController, QuestionController, Question};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +11,7 @@ Route::get('/', function () {
     }
 });
 
+Route::post('/question/like/{question}', Question\LikeController::class)->name('question.like');
 Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
 
 Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
